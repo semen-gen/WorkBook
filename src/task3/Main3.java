19e307deff4febe6e0f0a7de64b77720ff597254
Ivan <semen_gen@tut.by>
2020-04-15 12:25:32 +0300

    Add task 3

---
+++ src/task3/Main3.java
@@ -0,0 +1,12 @@
+package task3;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(isSevenLast(37));
+    }
+
+    public static boolean isSevenLast(int number) {
+        return number % 10 == 7;
+    }
+
+}
