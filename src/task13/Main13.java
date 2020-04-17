75f2f3874a04c68a31d73889c6b76935a3374bcf
Ivan <semen_gen@tut.by>
2020-04-17 22:28:47 +0300

    Add task 13

---
+++ src/task13/Main13.java
@@ -0,0 +1,32 @@
+package task13;
+
+public class Main13 {
+
+    public static void main(String[] args) {
+        simple(121);
+    }
+
+    public static void simple(int x) {
+        boolean simpleInt = true;
+
+        if (x > 0) {
+            for (int i = 2; i <= Math.sqrt(x); i++) {
+                if (!(x == 1 || x == 2) && x != i && x % i == 0) {
+                    simpleInt = false;
+                    break;
+                }
+            }
+        }
+        else {
+            simpleInt = false;
+        }
+
+        if (simpleInt) {
+            System.out.println("Простое");
+        }
+        else {
+            System.out.println("Не простое");
+        }
+    }
+
+}
